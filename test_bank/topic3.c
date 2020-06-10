4f4d27ad1f3265ac562728999b09ca1dfbaf0fd1
ximengyi <1203669692@qq.com>
2020-06-11 02:35:12 +0800

    topic3

--- test_bank/topic3.c
+++ test_bank/topic3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+//abcadfbb
 
 //判断字符是否在字符串数组中
  int in_array(char c ,char *s){
@@ -14,27 +14,62 @@
  }
 
  int lengthOfLongestSubstring(char * s){
-   int index =0;
+   int i;
+   int max =0;
+   int start =0;
+   int count =0;
+   int index [128] ={0};
    for (int i = 0; s[i] !='\0'; ++i)
    {
-      printf("%c\n", (s[i]));
+
+       if(index[s[i]] > start){
+         count = i-start;
+
+         if(max < count) max = count;
+         start = index[s[i]];
+       }
+
+        index[s[i]] =i+1;  
+        
+      }
+      count = i-start;
+      printf("%s\n", max);
+      return max;
    } 
-      return 0;
+      
+
+int lengthOfLongestSubstring2(char * s){
+    int i, j = 0, count = 0, max = 0, index[128] = {0}, start = 0;
+    for(i=0;s[i]!='\0';i++)     
+    {
+        if(index[s[i]]>start)   //index用来储存出现重复字符时
+        {                       //子串起始下标应移动到的地方
+            count = i-start;
+            if(count>max)
+            {
+                max = count;
+            }
+            start = index[s[i]];
+            printf("%d\n",i);
+            printf("%d\n", start);
+            printf("%s\n", "===========");
+        }
+        index[s[i]] = i+1;
+    }
+    count = i-start;
+    return count>max?count:max;
 }
 
 
 int main(int argc, char const *argv[])
 {
+  
+   //char s[9] = {'a','b','c','a','b','c','b','b'};
+   char s[9] = {'a','b','c','a','d','f','b','b'};
+  
+   int res = lengthOfLongestSubstring2(s);
 
-   char s[9] = {'a','b','c','a','b','c','b','b'};
 
-   if(in_array('0',s)){
-    printf("%s\n","是" );
-   }else{
-    printf("%s\n","否" );
-   }
-
-   lengthOfLongestSubstring(s);
    // int count = strlen(s);
    //    for (int i = 0; i <= count; ++i)
    // {
